38c81e270b6a3b3caf298baa4cfb4893596df8f3
TheSadNumanator <thesadnumanator@gmail.com>
2019-09-14 16:10:54 -0700

    Haiku checker
    if channel is named "haiku-hell" if anything in it is not a haiku it will be removed

--- Core/Program.cs
+++ Core/Program.cs
@@ -31,7 +31,7 @@ namespace DiscordDestrucoBot
         private CommandService _commands;
         private AudioService _audioservice;
         private IServiceProvider _services;
-         
+
         public async Task RunBotAsync()
         {
             if (Config.bot.token == "" || Config.bot.token == null)
@@ -105,49 +105,58 @@ namespace DiscordDestrucoBot
             if (message.HasStringPrefix(DataStorage.GetPrefixValue(contex.Guild.Id.ToString()), ref argPos) || message.HasMentionPrefix(_client.CurrentUser, ref argPos))
             {
 
-                var result = await _commands.ExecuteAsync(contex, argPos, _services);
-
+                var result = await _commands.ExecuteAsync(contex, argPos, _services);//execute command if it exists
 
                 if (result.IsSuccess)
                     return;
                 else
                 {
-                    if(result.Error != CommandError.UnknownCommand)
+                    if (result.Error != CommandError.UnknownCommand)
                         await contex.Channel.SendMessageAsync(result.ErrorReason);
                 }
 
 
             }
-                /* Tutorial:
+            /* Tutorial:
 
-                await contex.Channel.SendMessageAsync("Hello World."); //  This allows you to send a string as a message into discord.
-                //you can check if the messagestring is == to whatever command you want such as "!cat".
-                //using other normal C# code works fine, all you need to know is the send message thing.
-                
-                //But of course here is a little more information.
-                
-                //Here you create the class that creates embeded builders
-                EmbedBuilder builder = new EmbedBuilder();
+            await contex.Channel.SendMessageAsync("Hello World."); //  This allows you to send a string as a message into discord.
+            //you can check if the messagestring is == to whatever command you want such as "!cat".
+            //using other normal C# code works fine, all you need to know is the send message thing.
 
-                builder.WithTitle($"I AM EMBED TITLE")
-                    .WithColor(Color.Blue)
-                    .AddField("I AM TITLE", "I AM DESCRIPTION");
+            //But of course here is a little more information.
 
-                //You can just add .AddField to add another field, remove the color, or do some other stuff by just adding . to the end of the builder.
-                //After you have built the embed you like use this to message it into discord.
+            //Here you create the class that creates embeded builders
+            EmbedBuilder builder = new EmbedBuilder();
 
-                await ReplyAsync("", false, builder.Build());//This will build the embed and put it in discord as a message.
-                
+            builder.WithTitle($"I AM EMBED TITLE")
+                .WithColor(Color.Blue)
+                .AddField("I AM TITLE", "I AM DESCRIPTION");
 
-                //If you want contex about who where why and how the message was sent.
-                //Use contex.Whatever
-                //For example //contex.User.Username; //will get the username of the person who sent the message.
-                //contex.Message.Reactions;//will get all the reactions that the message has (although you'd be checking a message as it was created so it'd get nothing)
-                 //just try contex.    then look
-                 * * */
+            //You can just add .AddField to add another field, remove the color, or do some other stuff by just adding . to the end of the builder.
+            //After you have built the embed you like use this to message it into discord.
 
-                
-                string messagestring = message.Content.ToLowerInvariant();//convert the message to lowercase and put it in a string
+            await ReplyAsync("", false, builder.Build());//This will build the embed and put it in discord as a message.
+
+
+            //If you want contex about who where why and how the message was sent.
+            //Use contex.Whatever
+            //For example //contex.User.Username; //will get the username of the person who sent the message.
+            //contex.Message.Reactions;//will get all the reactions that the message has (although you'd be checking a message as it was created so it'd get nothing)
+             //just try contex.    then look
+             * * */
+
+
+            string messagestring = message.Content.ToLowerInvariant();//convert the message to lowercase and put it in a string
+
+            if (contex.Channel.Name == "haiku-hell")
+            {
+                //await contex.Channel.SendMessageAsync(isThisHaiku(messagestring) ? "true" : "false");
+                if (!isThisHaiku(messagestring))
+                {
+                    await contex.Message.DeleteAsync();
+                    return;
+                }
+            }
 
             if (messagestring == "999")
             {
@@ -157,7 +166,6 @@ namespace DiscordDestrucoBot
             {
                 await contex.Channel.SendMessageAsync("**No**");
             }
-
         }
 
 
@@ -165,7 +173,62 @@ namespace DiscordDestrucoBot
 
 
 
+        bool isThisHaiku(string possible_haiku)
+        {
+            if (possible_haiku.Length > 1)//if the message is more than one character
+            {
+                //SyllableCount
 
+                string[] strarray = possible_haiku.Split("\r\n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);//Split message by nextline
+
+                if (strarray.Length != 3)//if this is not three lines
+                {
+                    return false;//This is not a proper haiku
+                }
+
+                for (uint i = 0; i < strarray.Length; i++)//for every sentence in the possible haiku
+                {
+                    char[] arr = strarray[i].Where(c => (char.IsLetter(c) ||
+                             char.IsWhiteSpace(c))).ToArray();
+
+                    strarray[i] = new string(arr);
+
+                    string[] wordarray = strarray[i].Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);//seperate each word into array
+
+                    int Syllablecount = 0;
+                    for (uint j = 0; j < wordarray.Length; j++)//Count every syllable in sentence
+                    {
+                        int syllableword = 0;
+
+                        syllableword += SyllableCount(wordarray[j]);
+
+                        Syllablecount += syllableword;
+                    }
+
+                    if (i == 1)//If middle line
+                    {
+                        if (Syllablecount != 7)//Must be 7
+                        {
+                            return false;
+                        }
+                    }
+                    else//If first or last line
+                    {
+                        if (Syllablecount != 5)//Must be 5
+                        {
+                            return false;
+                        }
+                    }
+
+                }
+            }
+            else//message not more than one character is of course not a haiku
+            {
+                return false;
+            }
+
+            return true;
+        }
 
         static string RemoveDiacritics(string text)
         {
@@ -182,6 +245,40 @@ namespace DiscordDestrucoBot
             }
 
             return stringBuilder.ToString().Normalize(NormalizationForm.FormC);
+        }
+
+
+        private static int SyllableCount(string word)
+        {
+
+            word = word.ToLower().Trim();
+            bool lastWasVowel = false;
+            var vowels = new[] { 'a', 'e', 'i', 'o', 'u', 'y' };
+            int count = 0;
+
+            //a string is an IEnumerable<char>; convenient.
+            foreach (var c in word)
+            {
+                if (vowels.Contains(c))
+                {
+                    if (!lastWasVowel)
+                        count++;
+                    lastWasVowel = true;
+                }
+                else
+                    lastWasVowel = false;
+            }
+
+            if ((word.EndsWith("e") || (word.EndsWith("es") || word.EndsWith("ed")))
+                  && !word.EndsWith("le"))
+                count--;
+
+            if (count == 0)//If the word is somehow 0 syllables (be and ree are) 
+                count = 1;//make sure it has at least one
+
+            //Console.WriteLine(word + "=" + count);
+
+            return count;
         }
     }
 }
